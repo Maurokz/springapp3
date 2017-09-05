d6933d5e33ea7ae3518e8ee486e17042be46fdcd
mauro <mauro@DESKTOP-BITG8SM>
2017-09-05 15:07:31 -0300

    primer commit

---
+++ springapp3/src/main/resources/es/ua/jtech/spring/negocio/IPedidosBO.java
@@ -0,0 +1,8 @@
+
+public interface IPedidosBO {
+	
+	public void insertarPedido(int idCliente,int idProducto,int unidades)
+	public void setCantidadMaxima(int max)
+	public int getCantidadMaxima()
+
+}
